275ff43d3249391d1051f8f0f25893f341a8bdfd
PICARDBRB <43149675+PICARDBRB@users.noreply.github.com>
2018-10-21 14:54:47 -0400

    Add files via upload

--- assets/ik_tabs.js
+++ assets/ik_tabs.js
@@ -61,7 +61,7 @@
 				$tab = $('<li/>').attr({
 					'id': id + '_tab' + i, // create unique id for a tab
 					'role': 'tab', // assign tab role
-    			'aria-controls': 'panel' + i // define which panel it controls
+    			'aria-controls':'panel' + i // define which panel it controls
 
 				})
 				.text(lbl > '' ? lbl : 'Tab ' + (i + 1))
@@ -114,7 +114,8 @@
         tabindex: 0
     });
 		
-		if (event.type) $($tabs[ind]).focus(); // move focus to current tab if reached by mouse or keyboard
+		//if (event.type) $($tabs[ind]).focus(); // move focus to current tab if reached by mouse or keyboard
+		$($tabs[ind]).focus(); 
 		
 		$panels // hide all panels
 		.attr({
